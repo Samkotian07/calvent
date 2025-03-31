9389a0fbcc5b439efe02f3ffb695a12e2efd35e0
Samkotian07 <151937595+Samkotian07@users.noreply.github.com>
2025-03-31 18:06:48 +0530

    push

--- assets/js/main.js
+++ assets/js/main.js
@@ -176,3 +176,13 @@
 
 })();s
 
+
+function subscribe() {
+  let email = document.getElementById("email").value;
+  if (email) {
+      alert("Thank you for subscribing ");
+      document.getElementById("email").value = "";
+  } else {
+      alert("Please enter a valid email address.");
+  }
+}
